6e84d17a7bfd2506285cff5360cb99b2f523305f
francisco <io.franciscogomez@gmail.com>
2022-08-07 16:27:51 -0700

    Able to programmatically change the image of the ImageView from the ImageSelector. For example using a library like Glide.

--- app/src/main/java/com/example/mycustomeviews/MainActivity.java
+++ app/src/main/java/com/example/mycustomeviews/MainActivity.java
@@ -2,15 +2,28 @@ package com.example.mycustomeviews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+
+    private String[] imagesUrls =  new String[]{
+            "https://pbs.twimg.com/profile_images/1510276954938920966/_jPqrcHD_400x400.jpg",
+            "https://c.tenor.com/BSamNpi39MIAAAAM/minions.gif"
+    };
+    private Uri photoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        photoUri = Uri.parse(imagesUrls[1]);
         setContentView(R.layout.activity_main);
+       imageView = ((ImageSelector) findViewById(R.id.image_selector)).getSelectorImage();
+       Glide.with(this).load(photoUri).into(imageView);
 
-       // ((ImageSelector) findViewById(R.id.image_selector)).setBackground(R.drawable.placeholder);
     }
 }
